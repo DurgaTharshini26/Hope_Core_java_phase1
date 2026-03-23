4f09462dd5aa5a1db736fb52e8ca29b9d9c4fe12
SwethaAnandhan <swethapawin@gmail.com>
2026-03-23 19:49:23 +0530

    Added hope phase_1 tasks

---
+++ day_1/pattern1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pattern1{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<i+1;j++)
+            {
+                System.out.print("*");//same for 11(print i) and print j other pattern
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ day_2/LC_191.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n>0)
+        {
+            int re=n%2;
+            if(re==1)
+            {
+                count++;
+            }
+            n/=2;
+        }
+        return count;
+    }
+}
